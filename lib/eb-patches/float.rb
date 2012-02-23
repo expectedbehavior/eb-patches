4df95df7bc8834808399ba81cb5c78eb368981a4
Joel Meador <joel@expectedbehavior.com>
2012-02-23 08:32:12 -0500

    flatten the code a little for the float patch

--- lib/eb-patches/float.rb
+++ lib/eb-patches/float.rb
@@ -1,17 +1,13 @@
-module Eb
-  module Patches
-    module Float
-      module Approx
-        # Is a float within the EPSILON value of the Float base class?
-        # use: (0.0000000000000000000000001).approx(0) => probably true
-        # use: (-0.000002).approx(0) => false
-        def approx(other, relative_epsilon=::Float::EPSILON, epsilon=::Float::EPSILON)
-          difference = other - self
-          return true if difference.abs <= epsilon
-          relative_error = (difference / (self > other ? self : other)).abs
-          relative_error <= relative_epsilon
-        end
-      end
+module Eb::Patches::Float
+  module Approx
+    # Is a float within the EPSILON value of the Float base class?
+    # use: (0.0000000000000000000000001).approx(0) => probably true
+    # use: (-0.000002).approx(0) => false
+    def approx(other, relative_epsilon=::Float::EPSILON, epsilon=::Float::EPSILON)
+      difference = other - self
+      return true if difference.abs <= epsilon
+      relative_error = (difference / (self > other ? self : other)).abs
+      relative_error <= relative_epsilon
     end
   end
 end
