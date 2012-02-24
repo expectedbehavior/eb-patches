509880451f9ba4cbd9695844c5cd7dbe0dcf07bd
Joel Meador <joel@expectedbehavior.com>
2012-02-24 10:13:58 -0500

    refactor how files are required for more isolated running

--- lib/eb-patches.rb
+++ lib/eb-patches.rb
@@ -1,8 +1,3 @@
-require "eb-patches/version"
-require "eb-patches/float"
-require "eb-patches/string"
-require "active_support/core_ext/string/inflections"
-
 module Eb
   ###
   #  Usage:
@@ -11,12 +6,34 @@ module Eb
   #    Eb.monkey_patch Float, :approx
   ###
   def Eb.monkey_patch(patch_target, method_name)
-    camelized_method_name = method_name.to_s.gsub(/_/,' ').split.map(&:capitalize).join
-    patch_module = "Eb::Patches::#{patch_target}::#{camelized_method_name}"
-    patch_target.send :include, patch_module.constantize
+    require_active_support_string_helpers
+    require_patch_file_for_class(patch_target)
+    patch_module = module_to_include(patch_target, method_name)
+    patch_target.send :include, patch_module
   end
 
   def Eb.down_under(string)
     string.strip.gsub(/\s/, "_").gsub(/\W/, "").underscore.downcase
+  end
+
+  def Eb.require_active_support_string_helpers
+    require "active_support/core_ext/string/inflections"
+  end
+
+  def Eb.require_patch_file_for_class(klass)
+    require "eb-patches/#{down_under(klass.to_s)}"
+  end
+
+  def Eb.module_to_include(patch_target, method_name)
+    module_name = submodule_name(patch_target, method_name)
+    module_name.constantize
+  end
+
+  def Eb.submodule_name(patch_target, method_name)
+    "Eb::Patches::#{patch_target}::#{modulize_method_name(method_name)}"
+  end
+
+  def Eb.modulize_method_name(name)
+    name.to_s.gsub(/_/,' ').gsub(/[?!]/,'').split.map(&:capitalize).join
   end
 end
